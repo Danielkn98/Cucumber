832d3128b2cf826b0743e93aa6163d90359eae16
Daniil Korsun <danielkn98additional@gmail.com>
2021-12-10 23:50:15 +0300

    New repository

---
+++ src/test/java/Task20Cucumber/RunSpecs.java
@@ -0,0 +1,10 @@
+package Task20Cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty"})
+public class RunSpecs {
+}
